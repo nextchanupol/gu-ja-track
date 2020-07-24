732fb904a2a1f12cd6244f1e7ca1fa9bdf8d60e2
Chanupol Phermphoon <chanupol.gotoceo@gmail.com>
2020-07-24 12:11:36 +0700

    add pageview in started screen

--- lib/screens/welcome/started_screen.dart
+++ lib/screens/welcome/started_screen.dart
@@ -2,70 +2,76 @@ import 'package:flutter/material.dart';
 import 'package:learning_platform_app/models/slide_model.dart';
 import 'package:learning_platform_app/utils/images_util.dart';
 import 'package:learning_platform_app/utils/size_config.dart';
+import 'package:learning_platform_app/utils/strings.dart';
 import 'package:learning_platform_app/utils/styling.dart';
 import 'package:learning_platform_app/widgets/slide_item.dart';
 
 class StartedScreen extends StatelessWidget {
-  final PageController _pageController = new PageController(
-    initialPage: 0
-  );
+  final PageController _pageController = new PageController(initialPage: 0);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          child: Padding(
-            padding:
-                EdgeInsets.symmetric(vertical: 1 * SizeConfig.heightMultiplier),
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Expanded(
-                  flex: 3,
-                  child: PageView.builder(
-                    scrollDirection: Axis.horizontal,
-                    controller: _pageController,
-                    itemCount: slideListArr.length,
-                    itemBuilder: (context, index) => SlideItem(index),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 4,
+              child: Align(
+                alignment: Alignment.center,
+                child: Padding(
+                  padding:
+                      EdgeInsets.only(top: 4 * SizeConfig.heightMultiplier),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Expanded(
+                        child: PageView.builder(
+                          scrollDirection: Axis.horizontal,
+                          controller: _pageController,
+                          itemCount: slideListArr.length,
+                          itemBuilder: (context, index) => SlideItem(index),
+                        ),
+                      ),
+                      SizedBox(height: 3 * SizeConfig.heightMultiplier),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 5 * SizeConfig.heightMultiplier),
+                            child: FlatButton(
+                              onPressed: () {},
+                              child: Text('Getting Started'),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(8.0)),
+                              color: Theme.of(context).primaryColor,
+                              padding: EdgeInsets.all(
+                                  1.8 * SizeConfig.heightMultiplier),
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text(
+                                'Have an account ?',
+                                style: Theme.of(context).textTheme.subtitle2,
+                              ),
+                              FlatButton(
+                                onPressed: null,
+                                child: Text('Login'),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ],
                   ),
                 ),
-                SizedBox(height: 3 * SizeConfig.heightMultiplier),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.symmetric(
-                          horizontal: 5 * SizeConfig.heightMultiplier),
-                      child: FlatButton(
-                        onPressed: () {},
-                        child: Text('Getting Started'),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8.0)),
-                        color: Theme.of(context).primaryColor,
-                        padding:
-                            EdgeInsets.all(1.8 * SizeConfig.heightMultiplier),
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text(
-                          'Have an account ?',
-                          style: Theme.of(context).textTheme.subtitle2,
-                        ),
-                        FlatButton(
-                          onPressed: null,
-                          child: Text('Login'),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              ],
+              ),
             ),
-          ),
+          ],
         ),
       ),
     );
--- lib/widgets/slide_item.dart
+++ lib/widgets/slide_item.dart
@@ -9,34 +9,56 @@ class SlideItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(
-          width: SizeConfig.imageSizeMultiplier * 50,
-          height: SizeConfig.imageSizeMultiplier * 50,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            image: DecorationImage(
-              image: AssetImage(slideListArr[index].imagePath),
-              fit: BoxFit.cover,
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+              flex: 1,
+              child: Padding(
+                padding: EdgeInsets.only(top: 1 * SizeConfig.heightMultiplier),
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: FittedBox(
+                    child: Text(
+                      slideListArr[index].title,
+                      style: Theme.of(context).textTheme.headline6,
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              )),
+          Expanded(
+            flex: 3,
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                  vertical: 1 * SizeConfig.heightMultiplier),
+              child: Image.asset(
+                slideListArr[index].imagePath,
+                fit: BoxFit.fill,
+              ),
             ),
           ),
-        ),
-        Padding(
-          padding: EdgeInsets.only(bottom: 1 * SizeConfig.heightMultiplier),
-          child: Text(
-            slideListArr[index].title,
-            style: Theme.of(context).textTheme.headline6,
+          Expanded(
+            flex: 1,
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: FittedBox(
+                child: Padding(
+                  padding:
+                      EdgeInsets.only(bottom: 2 * SizeConfig.heightMultiplier),
+                  child: Text(
+                    slideListArr[index].description,
+                    style: Theme.of(context).textTheme.subtitle2,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ),
           ),
-        ),
-        Padding(
-          padding: EdgeInsets.only(bottom: 0.5 * SizeConfig.heightMultiplier),
-          child: Text(
-            slideListArr[index].description,
-            style: Theme.of(context).textTheme.subtitle2,
-          ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
